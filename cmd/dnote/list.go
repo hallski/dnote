589cfe0708e35968fc465d52588b9e77c31531ca
Mikael Hallendal <hallski@hallski.org>
2023-11-02 21:13:06 +0100

    Ellipsize the title when longer than 42 chars

--- cmd/dnote/list.go
+++ cmd/dnote/list.go
@@ -6,12 +6,30 @@ import (
 	"os"
 	"strings"
 	"text/tabwriter"
+	"unicode"
 
 	"github.com/fatih/color"
 )
 
 type NoteLister interface {
 	ListNotes() []*dnote.Note
+}
+
+// https://stackoverflow.com/a/73939904
+func ellipticalTruncate(text string, maxLen int) string {
+	lastSpaceIx := maxLen
+	len := 0
+	for i, r := range text {
+		if unicode.IsSpace(r) {
+			lastSpaceIx = i
+		}
+		len++
+		if len > maxLen {
+			return text[:lastSpaceIx] + "..."
+		}
+	}
+	// If here, string is shorter or equal to maxLen
+	return text
 }
 
 func List(lister NoteLister) {
@@ -22,7 +40,7 @@ func List(lister NoteLister) {
 	for _, note := range lister.ListNotes() {
 		fmt.Fprintf(w, "%s\t%s\t%s\n",
 			idColor("%d", note.Id),
-			note.Title,
+			ellipticalTruncate(note.Title, 42),
 			tagColor(strings.Join(note.Tags, ", ")))
 	}
 

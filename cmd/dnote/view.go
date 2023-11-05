68aaf5c1b3a22d5e74ac6b002c75878c44422e49
Mikael Hallendal <hallski@hallski.org>
2023-11-05 22:53:20 +0100

    Render view with Dracula instead of dark
    Better looking.

--- cmd/dnote/view.go
+++ cmd/dnote/view.go
@@ -8,7 +8,7 @@ import (
 )
 
 func View(note *dnote.Note) {
-	out, err := glamour.Render(note.Content, "dark")
+	out, err := glamour.Render(note.Content, "dracula")
 	if err != nil {
 		fmt.Printf("Failed to render note: %s\n", note.ID)
 	}

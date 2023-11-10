d5a2faa97e04281c71784483f0d947d234d327ef
Mikael Hallendal <hallski@hallski.org>
2023-11-10 16:06:21 +0100

    Rename open to edit

--- cmd/edit.go
+++ cmd/edit.go
@@ -14,7 +14,7 @@ type NoteFinder interface {
 	FindNote(id string) *core.Note
 }
 
-func Open(id string, finder NoteFinder) error {
+func Edit(id string, finder NoteFinder) error {
 	note := finder.FindNote(id)
 	if note == nil {
 		return fmt.Errorf("Couldn't find note %s", id)
@@ -27,13 +27,14 @@ func Open(id string, finder NoteFinder) error {
 	return nil
 }
 
-var openCmd = &cobra.Command{
-	Use:   "open",
-	Short: "Open a note",
-	Long:  "Opens note with ID in Vim",
+var editCmd = &cobra.Command{
+	Use:     "edit",
+	Aliases: []string{"open"},
+	Short:   "Edit a note",
+	Long:    "Opens note with ID in $EDITOR",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) >= 1 {
-			return Open(mdfiles.PadID(args[0]), notes)
+			return Edit(mdfiles.PadID(args[0]), notes)
 		} else {
 			return ext.OpenEditor()
 		}
@@ -41,5 +42,5 @@ var openCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(openCmd)
+	rootCmd.AddCommand(editCmd)
 }

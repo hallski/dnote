89acd907e69bf52e56a8a7a9ad5caf10c0a74634
Mikael Hallendal <hallski@hallski.org>
2023-11-20 01:17:43 +0100

    Tone down highlight in bargraphics

--- render/bargraphics.go
+++ render/bargraphics.go
@@ -3,7 +3,7 @@ package render
 import "github.com/charmbracelet/lipgloss"
 
 func BarGraphics(text string) string {
-	highlight := lipgloss.NewStyle().Foreground(lipgloss.Color("#562fd0"))
+	highlight := lipgloss.NewStyle().Foreground(lipgloss.Color("#4c2ab9"))
 	textColor := StyleDivider.Copy().Foreground(ColorWhite).Background(DividerColor)
 
 	return StyleDivider.Render("▀ ") +
